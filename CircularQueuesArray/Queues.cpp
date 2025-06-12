1d4d56f370ed027337a0d019283a9021da7a8814
rizkiadp <sukmahawaiza@gmail.com>
2025-06-12 15:58:48 +0700

    add switch case untuk sesuai pilihan

--- CircularQueuesArray/Queues.cpp
+++ CircularQueuesArray/Queues.cpp
@@ -133,7 +133,37 @@ int main()
             cin >> ch;
             cout << endl;
 
-           
+            switch (ch)
+            {
+            case '1':
+            {
+                q.insert();
+                break;
+            }
+            case '2':
+            {
+                q.remove();
+                break;
+            }
+            case '3':
+            {
+                q.display();
+                break;
+            }
+            case '4':
+            {
+                return 0;
+            }
+            default:
+            {
+                cout << "Invaid option!" << endl;
+                break;
+            }
+            }
+        }
+        catch (exception &e)
+        {
+            cout << "Check for the values entered." << endl;
         }
     }
 };
