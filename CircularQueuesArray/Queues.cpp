269dfdc848867e20feee4a755dfdbbcf818e0cf1
rizkiadp <sukmahawaiza@gmail.com>
2025-06-12 15:38:49 +0700

    jika elemen yang dihapus berada di poisi terakhir, kembali ke awal  array

--- CircularQueuesArray/Queues.cpp
+++ CircularQueuesArray/Queues.cpp
@@ -62,6 +62,15 @@ public:
             FRONT = -1;
             REAR = -1;
         }
-        
+        else
+        {
+            // jika elemen yang dihapus berada di poisi terakhir, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
     }
+
+    
 };
