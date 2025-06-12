576e87b18c954eacc3d066cc24da9f9bda8da822
rizkiadp <sukmahawaiza@gmail.com>
2025-06-12 15:33:02 +0700

    add step 1.a dan 1.b

--- CircularQueuesArray/Queues.cpp
+++ CircularQueuesArray/Queues.cpp
@@ -25,7 +25,8 @@ public:
         // 1. cek apakah antrian penuh
         if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
         {
-            
+            cout << "\nQueue overflow\n"; // 1.a
+            return;                       // 1.b
         }
 
         
