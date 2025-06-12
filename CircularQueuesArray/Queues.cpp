abcc17b605807aabe80023fe038d12af5dcac3ee
rizkiadp <sukmahawaiza@gmail.com>
2025-06-12 15:35:18 +0700

    jika REAR berada di posisi terakhitr array, kembali ke awal array

--- CircularQueuesArray/Queues.cpp
+++ CircularQueuesArray/Queues.cpp
@@ -35,6 +35,17 @@ public:
             FRONT = 0; // 2.a
             REAR = 0;  // 2.b
         }
-        
+        else
+        {
+            // jika REAR berada di posisi terakhitr array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
     }
+
+    
+
 };
